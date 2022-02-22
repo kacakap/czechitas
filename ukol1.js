604663f2fe7be1a41475de5251bbd6dbfa047a4d
kacakap <kapounova.katerina@zsbcuo.cz>
2022-02-22 18:18:21 +0100

    1 file modified

--- ukol1.js
+++ ukol1.js
@@ -3,4 +3,4 @@ let věk = 21;
 let váha = 56;
 let výška = 165;
 let bmi = váha / výška
-console.log("Jmenuji se "+ jméno +", je mi " + věk +", vážím " + váha + " kg a měřím " + výška + " cm, moje bmi je " + bmi + "." )
+console.log("Jmenuji se "+ jméno +", je mi " + věk +" let, vážím " + váha + " kg a měřím " + výška + " cm, moje bmi je " + bmi + "." )
